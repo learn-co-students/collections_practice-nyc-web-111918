1e304b91f4ca404d794eea5fa458080ccf398c97
cfinn16 <connorfinnegan16@gmail.com>
2018-11-20 16:52:58 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |string| string.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |string|
+    string[2] = "$"
+    string
+  end
+end
+
+def find_a(array)
+  array.select {|word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.map do |word|
+    if word == "feet"
+      "feet"
+    else
+      word << "s"
+    end
+  end
+end
