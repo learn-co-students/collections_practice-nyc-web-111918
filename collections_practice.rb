064e5f6b004452a4304f46a022fd551c67e8f0d7
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-20 04:18:59 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort_by {|el| el.length }
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def swap_elements(array)
+  [array[0], array[2], array[1]]
+end 
+
+def kesha_maker(array)
+  array.map do |word|
+    c = word.chars
+    c[2] = "$"
+    c.join    
+  end 
+end 
+
+def find_a(array)
+  array.select {|word| word[0] == "a"}
+end 
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.map do |word| 
+    if word == array[1]
+      word
+    else
+     word + "s" 
+   end 
+  end 
+end 
